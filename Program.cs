57ed783a2237ee0d52b201a03a63e2b5c54f3f3a
chrisokhamera <chrisokhamera@gmail.com>
2020-01-09 16:44:14 +0000

    First Stage Update
    The first stage has been completed. The software now reads snort logfiles. This was successfuly tested on Ubuntu operating system

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using snortdb;
+using System.Collections;
+
+namespace razor
+{
+    public static class ProgramData
+    {
+        public static string snort_conf = Startup.Configuration.GetSection("Sources:snort_conf").Value;
+    }
+
+    public class Program
+    {
+        public static string rootpath;
+        public static void Main(string[] args)
+        {
+            rootpath = AppContext.BaseDirectory;
+            #if DEBUG
+                    rootpath = Directory.GetCurrentDirectory();
+            #endif
+
+            var configuration = new ConfigurationBuilder()
+            .AddCommandLine(args)
+            .Build();
+            CreateWebHostBuilder(args).UseConfiguration(configuration).Build().Run();
+            ;
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseKestrel()
+                .UseContentRoot(rootpath) //AppContext.BaseDirectory
+                .UseUrls("http://*:5000");
+
+    }
+}
